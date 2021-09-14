f7074465ed56a2f6077f7da617be7bf8d3c55151
Sandra <sandraantony2002@gmail.com>
2021-09-14 13:29:18 +0530

    thrid file uploaded

---
+++ Untitled-3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    int n;
+    printf("Ente a number");
+    scanf("%d",&n);
+    printf("The number you entered is %d",n);
+    return EXIT_SUCCESS;
+}
